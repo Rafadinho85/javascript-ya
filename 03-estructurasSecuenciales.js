2df62bfc7db9bb980de1530e0774035970a3f24d
Jery Ramirez <rafa85jogo@gmail.com>
2020-05-16 20:38:33 -0600

    estructura secuencial

---
+++ 03-estructurasSecuenciales.js
@@ -0,0 +1,13 @@
+'use strict'
+/*
+Cuando en un problema sólo participan operaciones, entradas y salidas 
+se la denomina estructura secuencial.
+*/
+var numero1 = parseInt(prompt("Ingrese un numero "));
+var numero2 = parseInt(prompt("Ingrese un segundo numero "));
+var suma = numero1+numero2;
+var multi = numero1*numero2;
+
+document.write("La suma es: " +suma);
+document.write('<br>');
+document.write("La multi es: " +multi);
